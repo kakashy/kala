1300188b7c2e1148f3a69a611f872e525dcb858b
kakashy <josephwawerug@gmail.com>
2021-12-03 05:00:21 +0300

    [chore] Added a service worker

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: vercel(),
+		files: {
+			serviceWorker: 'src/service-worker'
+		}
 	}
 };
 
